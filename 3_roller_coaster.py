b656c819068255d2c2d662038ad502ab433f78cd
Hilman <hpanser@gmail.com>
2024-09-19 06:01:26 +0700

    update

--- 3_roller_coaster.py
+++ 3_roller_coaster.py
@@ -1,15 +1,26 @@
 print("Welcom to the rollercoaster!")
 
 height = int(input("What is your height in cm? "))
+photos_fee = 3
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Kids under 12 bill is $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Teenagers bill is $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adults bill is $12.")
+
+    photos = input("Do you want photos taken? (Yes or No) ")
+    if photos == "Yes":
+        bill += photos_fee
+    
+    print(f"Your total bill is ${bill}.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
