998111715f9baea2538c2f40291a7090da13e6d9
Hilman <hpanser@gmail.com>
2024-09-19 20:39:37 +0700

    Create 3_love_calculator.py

---
+++ 3_love_calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to the Love Calculator!")
+
+your_name = input("What is your name: ").upper()
+# their_name = input("What is their name: ").upper()
+
+true_love = "TRUELOVE"
+# your_result = your_name.count("T")
+# your_result += your_name.count("R")
+# your_result += your_name.count("U")
+# your_result += your_name.count("E")
+# your_result += your_name.count("L")
+# your_result += your_name.count("O")
+# your_result += your_name.count("V")
+# your_result += your_name.count("E")
+
+for result in true_love:
+    your_name.count(result)
+
+
+# print(your_result)
+
