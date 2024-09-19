2c4cb0fdef368059e9b56b4ef760b539ee17679e
Hilman <hpanser@gmail.com>
2024-09-20 05:34:40 +0700

    Create 3_treasure_island.py

---
+++ 3_treasure_island.py
@@ -0,0 +1,7 @@
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("You're at a cross road. Where do you want to go? Type 'left or right'. ")
+print("You cam to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
+print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")
+print("You enter a room of beasts. Game Over!")
+
