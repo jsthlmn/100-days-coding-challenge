a76ce790a05f194ec13a00d655ee4db93edd3ebf
Hilman <hpanser@gmail.com>
2024-09-21 12:20:21 +0700

    Update 3_treasure_island.py

--- 3_treasure_island.py
+++ 3_treasure_island.py
@@ -1,7 +1,22 @@
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-print("You're at a cross road. Where do you want to go? Type 'left or right'. ")
-print("You cam to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
-print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")
-print("You enter a room of beasts. Game Over!")
+direction = input("You're at a cross road. Where do you want to go? Type 'left or right': ").lower()
 
+if direction == "left":
+    lake = input("You cam to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ").lower()
+    if lake == "wait":
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
+        if door == "yellow":
+            print("You Win!")
+        elif door == "red":
+            print("You enter a room of beasts. Game Over!")
+        elif door == "blue":
+            print("You enter a room of beasts. Game Over!")
+        else:
+            print("The you chose in not in the option. Game Over")
+    elif lake == "swim":
+        print("There is a shark in the water. Game Over")
+    else:
+        print("The direction you chose in not in the option. Game Over")
+else:
+    print("You chose the wrong direction. Game Over")
