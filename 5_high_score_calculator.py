dffcee8df4361b3b72964a8189cb73466a664582
Hilman <hpanser@gmail.com>
2024-09-24 06:26:56 +0700

    Create 5_high_score_calculator.py

---
+++ 5_high_score_calculator.py
@@ -0,0 +1,27 @@
+student_scores = input("Input a lists of student scores: ").split()
+highest_score = 0
+
+# Make string in list into int
+for i in range(0, len(student_scores)):
+    student_scores[i] = int(student_scores[i])
+
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(highest_score)
+
+
+# -----------OR-----------
+# ------Using selection sort-------
+
+# for ind in range(len(student_scores)):
+#     min_index = ind
+ 
+#     for j in range(ind + 1, len(student_scores)):
+#         # select the minimum element in every iteration
+#         if student_scores[j] < student_scores[min_index]:
+#             min_index = j
+#     # swapping the elements to sort the array
+#     (student_scores[ind], student_scores[min_index]) = (student_scores[min_index], student_scores[ind])
+# print(student_scores)
+# print(student_scores[-1])
