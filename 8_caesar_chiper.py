27ec04ee0f027d2b8ab8874f1229c868313299c3
Hilman <hpanser@gmail.com>
2024-09-30 13:24:09 +0700

    Create 8_caesar_chiper.py

---
+++ 8_caesar_chiper.py
@@ -0,0 +1,4 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+options = input("Type 'encode' to encrypt, type 'decode' to decrypt! :")
+message = input("Type your message: ")
+shift = int(input("Enter the shift number: "))
