38d780c13a802bcf8bac0c207780ca711abc14cd
hilman <hpanser@gmail.com>
2024-10-14 19:47:47 +0700

    update 9_dictionary_in_list.py

---
+++ 9_dictionary_in_list.py
@@ -0,0 +1,12 @@
+travel_log = [
+    {
+        "country": "france",
+        "cities_visited": ["paris", "lile", "dijon"],
+        "total_visited": 12
+    },
+    {
+        "country": "german"
+        "cities_visited": ["berlin", "hambrug", "stuttgard"],
+        "total_visited": 10
+    }
+]
