68b3477779100b9c62ae236c7ebd5bd725afeaa2
hilman <hpanser@gmail.com>
2024-10-16 19:42:44 +0700

    update 9_dictionary_in_list.py

--- 9_dictionary_in_list.py
+++ 9_dictionary_in_list.py
@@ -5,8 +5,19 @@ travel_log = [
         "total_visited": 12
     },
     {
-        "country": "german"
+        "country": "german",
         "cities_visited": ["berlin", "hambrug", "stuttgard"],
         "total_visited": 10
     }
 ]
+
+def add_new_dictionary(counrty_visited, time_visited, cities_visited):
+    new_dictionary = {}
+    new_dictionary["country"] = counrty_visited
+    new_dictionary["cities_visited"] = cities_visited
+    new_dictionary["total_visited"] = time_visited
+
+    travel_log.append(new_dictionary)
+
+add_new_dictionary("rusia", "15", ["moscow", "saint-petersburg", "kalingrad"])
+print(travel_log)
