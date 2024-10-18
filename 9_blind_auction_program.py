4a42bd540bb89df9632f8a4a6eec102be103f7b7
hilman <hpanser@gmail.com>
2024-10-18 20:59:59 +0700

    update 9_blind_auction_program.py

--- 9_blind_auction_program.py
+++ 9_blind_auction_program.py
@@ -3,17 +3,25 @@ from art import logo
 print(logo)
 print("Welcome to the secret auction program.")
 
-input_name = input("What is your name? ")
-input_bid = input("What is your bid? ")
+bids_data = {}
+bidding_finished = False
 
-bid_data = []
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with the bid of {highest_bid}")
 
-def add_new_bid_data(name, bid):
-    new_bid_dict = {}
-    new_bid_dict['name']= name
-    new_bid_dict['bid']= bid
+while not bidding_finished:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? "))
+    bids_data[name] = bid
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.")
 
-    bid_data.append(new_bid_dict)
-
-add_new_bid_data(input_name, input_bid)
-print(bid_data)
+    if should_continue == 'no':
+        bidding_finished = True
+        find_highest_bidder(bids_data)
