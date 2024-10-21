8e908aca7ef45f7e6d40e86d6754cf2365cd9202
hilman <hpanser@gmail.com>
2024-10-21 20:35:40 +0700

    update

--- 10_calculator.py
+++ 10_calculator.py
@@ -19,15 +19,25 @@ operations = {
     "/": divide
 }
 
-first_number = int(input("Insert the first number you want to calc: "))
+def calculator():
+    first_number = int(input("Insert the first number you want to calc: "))
 
-for i in operations:
-    print(i)
-select_operation = input("What operation you want to choose?")
-second_number = int(input("Insert the second number you want to calc: "))
+    for i in operations:
+        print(i)
 
-calc = operations[select_operation]
-result = calc(first_number, second_number)
+    should_continue = True
+    while should_continue == True:
+        select_operation = input("What operation you want to choose? ")
+        second_number = int(input("Insert the next number you want to calc: "))
 
-print(f"{first_number} {select_operation} {second_number} is {result}")
+        calc = operations[select_operation]
+        result = calc(first_number, second_number)
+
+        print(f"{first_number} {select_operation} {second_number} is {result}")
+        if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ") == 'y':
+            first_number = result
+        else:
+            should_continue = False
+            calculator()
     
+calculator()
