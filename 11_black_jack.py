5852dab97e16528afb761e6b1c02844a2fd23b9f
hilman <hpanser@gmail.com>
2024-10-22 20:41:05 +0700

    create 11_black_jack.py and black_jack_art.py

---
+++ 11_black_jack.py
@@ -0,0 +1,8 @@
+# from black_jack_art import logo
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+user = 0
+computer = 0
+input("Do you want to play a Blackjack game? Type 'y' or 'n' ")
