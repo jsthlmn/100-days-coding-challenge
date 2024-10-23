0a863b18ae16edd6d78ea6bf0cca0720d06104c7
hilman <hpanser@gmail.com>
2024-10-23 20:39:13 +0700

    update

--- 11_black_jack.py
+++ 11_black_jack.py
@@ -1,8 +1,36 @@
 # from black_jack_art import logo
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+    """Return a random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-user = 0
-computer = 0
-input("Do you want to play a Blackjack game? Type 'y' or 'n' ")
+def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
+    if 11 in cards and 10 in cards and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
+    return sum(cards)
+
+user = []
+computer = []
+is_game_over = False
+
+for _ in range(2):
+    user.append(deal_card())
+    computer.append(deal_card())
+
+user_score = calculate_score(user)
+computer_score = calculate_score(computer)
+
+print(f"Your cards: {user}, the current score is {user_score}")
+print(f"Computer's first card: {computer[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
