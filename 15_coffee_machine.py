a8cc3e20be19435c8c1a0c8e40b18fe27488ba81
hilman <hpanser@gmail.com>
2024-12-08 21:02:59 +0700

    update

--- 15_coffee_machine.py
+++ 15_coffee_machine.py
@@ -30,3 +30,13 @@ resources = {
     "coffee": 100,
 }
 
+# TODO: 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+# TODO: 2. Turn off the coffe machine by entering "off" to the prompt.
+# TODO: 3. Print report
+# TODO: 4. Check resources sufficient?
+# TODO: 5. Process coins
+# TODO: 6. Check transaction successful?
+# TODO: 7. Make Coffee.
+
+
+
