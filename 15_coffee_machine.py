b12fca99263b18a13a2d5bfa4f0cea0c6e7bc469
hilman <hpanser@gmail.com>
2024-12-10 21:06:25 +0700

    update

--- 15_coffee_machine.py
+++ 15_coffee_machine.py
@@ -30,13 +30,27 @@ resources = {
     "coffee": 100,
 }
 
+current_money = 0
+
 # TODO: 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+
+machine_off = False
+while not machine_off :
+    # Ask the user for the options
+    user_input = input("What would you like? (espresso/latte/cappuccino): ")
+    if user_input == 'off':
+        machine_off = True
+    elif user_input == 'report':
+        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: {current_money}")
+
 # TODO: 2. Turn off the coffe machine by entering "off" to the prompt.
 # TODO: 3. Print report
 # TODO: 4. Check resources sufficient?
 # TODO: 5. Process coins
 # TODO: 6. Check transaction successful?
 # TODO: 7. Make Coffee.
+
+
 
 
 
