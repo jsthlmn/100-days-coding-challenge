d7647ea099bdc17fea636930b0aa1dd691ecfa32
jsthlmn <hpanser@gmail.com>
2025-01-19 17:36:30 +0700

    Update main.py

--- 16_oop/main.py
+++ 16_oop/main.py
@@ -1,10 +1,23 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("cyan")
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("cyan")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+# table.field_names = ["Pokemon Name", "Type"]
+# table.add_row(["Pikachu", "Electric"])
+# table.add_row(["Squirtle", "Water"])
+# table.add_row(["Charmander", "Fire"])
+print(table)
